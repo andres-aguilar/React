053964cab463d548e6dcca74ec47fc4a1c1bacef
Andrés Aguilar <andresyoshimar@gmail.com>
2018-11-26 17:25:35 -0600

    Creando y renderizando primero componente

---
+++ src/components/media.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+class Media extends Component {
+    render() {
+        return (
+            <div>
+                <div>
+                    <img 
+                        src=""
+                        alt=""
+                        width={260}
+                        height={160}
+                    />
+                    <h3> Por qué aprender ReactJS </h3>
+                    <p> Andres Aguilar </p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Media
