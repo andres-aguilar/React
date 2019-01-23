1cca9157c3b73fad74e678c0ee3e7525e89c11d6
Andrés Aguilar <andresyoshimar@gmail.com>
2019-01-23 16:39:14 -0600

    Construyendo el proyecto para producción

---
+++ docs/precache-manifest.07000b4dd6e39a3c5c6ad64161c68ad4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a4acab461dc2be7ca724e3d136abf80",
+    "url": "/static/media/volume.9a4acab4.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7155da1c53311de1d6a7",
+    "url": "/static/js/main.7155da1c.chunk.js"
+  },
+  {
+    "revision": "2ccebacc9619d0f256ea",
+    "url": "/static/js/1.2ccebacc.chunk.js"
+  },
+  {
+    "revision": "7155da1c53311de1d6a7",
+    "url": "/static/css/main.1490ad95.chunk.css"
+  },
+  {
+    "revision": "6d5192fdb1c0e9780470acb122edb568",
+    "url": "/index.html"
+  }
+];
